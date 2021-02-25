94e091c8f9a0e6a5ccd8c49a2b2404b2d410723c
Mai Thanh Tam <thanhtam05.2015@gmail.com>
2021-02-25 12:03:17 +0700

    kdata test

---
+++ scripts/kdata/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>KDATA.VN VPS</title>
+<style>
+input {
+	width: 99%;
+	text-align: center;
+	padding: 10px 0;
+	border: 0 none;
+	font-size: 14px;
+	outline: none;
+}
+</style>
+</head>
+<body>
+<div>
+	<input type="text" value="curl -sO https://raw.githubusercontent.com/vuukhanhduy/kdata-vps/master/install && bash install" onClick="this.select();">
+</div>
+<br>
+<div>
+	<input type="text" value="yum -y install wget ; wget --no-check-certificate https://raw.githubusercontent.com/vuukhanhduy/kdata-vps/master/install ; chmod +x install ; bash install" onClick="this.select();">
+</div>
+</body>
+</html>
